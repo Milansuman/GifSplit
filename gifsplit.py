41db0e28b482ea9802a6f8477ef8218088777665
Milansuman <milansuman2016@gmail.com>
2023-02-21 18:26:48 +0530

    Implement hashing functionality for file handling

--- gifsplit.py
+++ gifsplit.py
@@ -1,6 +1,7 @@
 from flask import *
 from PIL import ImageSequence, Image
 import json
+import hashlib
 
 app = Flask(__name__)
 
@@ -18,12 +19,20 @@ def split():
     try:
         if request.method == 'POST':
             file = request.files['image']
+
+            # Getting the hash of the file contents. This can help avoid duplication
+            file_hash = hashlib.md5(file.stream.read()).hexdigest()
+
             with Image.open(file) as im:
                 for i, frame in enumerate(ImageSequence.Iterator(im)):
-                    frame.save(f"uploads/{file.filename}_{i}.png", format="PNG")
-                    frames.append(f"uploads/{file.filename}_{i}.png")
+
+                    #creating a hash for all the frames. The hashing helps with duplication issues.
+                    frame_hash = hashlib.md5(f"{file_hash}{i}".encode()).hexdigest()
+                    frame.save(f"uploads/{frame_hash}.png", format="PNG")
+                    frames.append(f"uploads/{frame_hash}.png")
 
         
         return json.dumps({"frames": frames})
-    except:
+    except Exception as e:
+        print(e)
         return make_response(json.dumps({}), 400)
